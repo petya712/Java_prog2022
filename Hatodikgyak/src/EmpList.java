f3d4c930e67ae8c7ee431b199f2ea6522a0b6e22
Szabó Péter <36503704+petya712@users.noreply.github.com>
2022-03-17 11:41:13 +0100

    Add files via upload

---
+++ Hatodikgyak/src/EmpList.java
@@ -0,0 +1,56 @@
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableRowSorter;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class EmpList extends JDialog {
+	
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private EmpTM etm;
+
+	
+	public EmpList(JFrame f, EmpTM betm) {
+		super(f, "Dolgozók listája",true);
+		etm = betm;
+		
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JButton btnBezar = new JButton("Bez\u00E1r");
+		btnBezar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnBezar.setBounds(161, 227, 89, 23);
+		getContentPane().add(btnBezar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 414, 205);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable(etm);
+		scrollPane.setViewportView(table);
+		
+		TableColumn tc = null;
+		for(int i =0; i< 6; i++){
+			tc = table.getColumnModel().getColumn(i);
+			if(i==0 || i==1 || i==5) tc.setPreferredWidth(30);
+			else{ tc.setPreferredWidth(100);}
+		}
+		table.setAutoCreateRowSorter(true);
+		TableRowSorter<EmpTM> trs = 
+				(TableRowSorter<EmpTM>)table.getRowSorter();
+				trs.setSortable(0, false);
+		
+
+	}
+}
