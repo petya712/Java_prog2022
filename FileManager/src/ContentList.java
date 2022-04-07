619dc07bcdff659826a39c1e30f0ec8e2ff7fd87
Szabó Péter <36503704+petya712@users.noreply.github.com>
2022-04-07 15:53:55 +0200

    FileSystem fully complete(1. és 2. rész)

--- FileManager/src/ContentList.java
+++ FileManager/src/ContentList.java
@@ -1,21 +1,27 @@
-import java.awt.EventQueue;
-
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
+
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
 import javax.swing.JScrollPane;
+import java.awt.Color;
 
 public class ContentList extends JDialog {
+	
+	private static final long serialVersionUID = 1L;
 	private String outDir="";
+	private String outFile="";
 	private JTable table;
 	private ListTM ltm;
+	private String separator = System.getProperty("file.separator");
 
 	
 	
@@ -42,6 +48,9 @@ public class ContentList extends JDialog {
 			boolean isd = list[i].isDirectory();
 			if (mod==1) ltm.addRow(new Object[]{false, list[i].getName(),isd});
 			if (mod==2 && isd) ltm.addRow(new Object[]{false, list[i].getName(),isd});
+			if (mod==3 && !isd) ltm.addRow(new Object[]{false, list[i].getName(),isd});
+			if ((mod==3 || mod==4) && !isd)
+				ltm.addRow(new Object[] {false, list[i].getName(),isd});
 		}
 		
 		
@@ -67,10 +76,89 @@ public class ContentList extends JDialog {
 			}
 		});
 		btnChange.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnChange.setBounds(20, 228, 89, 23);
+		btnChange.setBounds(236, 227, 89, 23);
 		getContentPane().add(btnChange);
-		if (mod == 2) btnChange.setVisible(true);
+		if (mod==2) btnChange.setVisible(true);
 		else btnChange.setVisible(false);
+		
+		JButton btnView = new JButton("View");
+		if (mod==1) btnView.setVisible(true);
+		else btnView.setVisible(false);
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int db=0, jel=0, x=0;
+				for(x = 0; x < ltm.getRowCount(); x++)
+					if((Boolean)ltm.getValueAt(x, 0)) {db++; jel=x;}
+				if(db==0) SM("No data row selected!",0);
+				
+				if(db>1) SM("Multiple rows selected!\nOnly select one!",0);
+				if(db==1) {
+					if((Boolean)ltm.getValueAt(jel, 2))
+						SM("The selected entry is not a file!",0);
+					else {
+						String fileName = ltm.getValueAt(jel, 1).toString();
+						File file = new File(inDir.getAbsolutePath()+separator+fileName);
+						try {
+							Desktop.getDesktop().open(file);
+						}catch (IOException ex) {
+							SM("No proper application to open the file!",0);
+						}
+					}
+				}
+			}
+		});
+		btnView.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnView.setBounds(137, 228, 89, 23);
+		getContentPane().add(btnView);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setForeground(Color.RED);
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int db=0, jel=0, x=0;
+				for(x = 0; x <ltm.getRowCount(); x++)
+				if((Boolean)ltm.getValueAt(x, 0)) {db++; jel=x;}
+				if(db==0) SM("No file selected!",0);
+				
+				if(db > 1) SM("Multiple rows selected!\nOnly select one!",0);
+				if(db == 1) {
+					String fileName=ltm.getValueAt(jel, 1).toString();
+					File delFile = new File(inDir.getAbsolutePath() + separator + fileName);
+					boolean ok = delFile.delete();
+					if(ok) {
+						ltm.removeRow(jel);
+						SM("File deleted!",1);
+					}
+				}
+			}
+		});
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnDelete.setBounds(10, 228, 117, 23);
+		getContentPane().add(btnDelete);
+		if (mod==3)btnDelete.setVisible(true);
+		else btnDelete.setVisible(false);
+		
+		JButton btnSelectToCopy = new JButton("Select to Copy");
+		btnSelectToCopy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int db=0, jel=0, x=0;
+				for(x=0;x<ltm.getRowCount();x++)
+				if((Boolean)ltm.getValueAt(x, 0)) { db++; jel=x;}
+				if(db==0) SM("No file selected!",0);
+				if(db>1) SM("Multiple rows selected!\nSelect only one!",0);
+				if(db==1) {
+					outFile= ltm.getValueAt(jel, 1).toString();
+					dispose();
+				}
+				
+			}
+		});
+		btnSelectToCopy.setForeground(Color.BLACK);
+		btnSelectToCopy.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSelectToCopy.setBounds(10, 228, 117, 23);
+		getContentPane().add(btnSelectToCopy);
+		if (mod == 4) btnSelectToCopy.setVisible(true);
+		else btnSelectToCopy.setVisible(false);
 		
 		TableColumn tc = null;
 		for (int i = 0; i < 3; i++){
@@ -86,6 +174,10 @@ public class ContentList extends JDialog {
 	}
 	
 	public void SM(String msg, int type){
-		JOptionPane.showMessageDialog(ContentList.this, msg,"FileSystem Manger üzenet", type);
+		JOptionPane.showMessageDialog(ContentList.this, msg,"FileSystem Manger Ã¼zenet", type);
+	}
+	
+	public String getOutFile() {
+		return outFile;
 	}
 }
--- FileManager/src/Program.java
+++ FileManager/src/Program.java
@@ -8,12 +8,20 @@ import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class Program extends JFrame {
 
@@ -25,6 +33,11 @@ public class Program extends JFrame {
 	private String CurDirText="";
 	private String separator = System.getProperty("file.separator");
 	private JTextField textNewDirName;
+	private JTextField textNewFileName;
+	private JTextField textSource;
+	private JTextField textDestination;
+	private String selectedFile="";
+	private File sourceFile, destFile;
 
 	/**
 	 * Launch the application.
@@ -48,15 +61,15 @@ public class Program extends JFrame {
 	public Program() {
 		setTitle("FileSystem Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 250);
+		setBounds(100, 100, 800, 576);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblCurrentFolder = new JLabel("Current folder:");
-		lblCurrentFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblCurrentFolder.setBounds(10, 27, 104, 14);
+		lblCurrentFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		contentPane.add(lblCurrentFolder);
 		
 		CurDir = new File(System.getProperty("user.dir"));
@@ -64,14 +77,15 @@ public class Program extends JFrame {
 		
 		
 		textCurFolder = new JTextField(CurDirText);
+		textCurFolder.setBounds(124, 25, 477, 20);
 		textCurFolder.setBackground(Color.WHITE);
 		textCurFolder.setEditable(false);
 		textCurFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		textCurFolder.setBounds(124, 25, 477, 20);
 		contentPane.add(textCurFolder);
 		textCurFolder.setColumns(10);
 		
 		JButton btnList = new JButton("List");
+		btnList.setBounds(10, 71, 89, 23);
 		btnList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ContentList ctl1 = new ContentList(Program.this, CurDir,1);
@@ -79,10 +93,10 @@ public class Program extends JFrame {
 			}
 		});
 		btnList.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnList.setBounds(10, 71, 89, 23);
 		contentPane.add(btnList);
 		
 		JButton btnChangeUpFolder = new JButton("Change Up Folder");
+		btnChangeUpFolder.setBounds(124, 72, 158, 23);
 		btnChangeUpFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
@@ -97,10 +111,10 @@ public class Program extends JFrame {
 		
 		
 		btnChangeUpFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnChangeUpFolder.setBounds(124, 72, 158, 23);
 		contentPane.add(btnChangeUpFolder);
 		
 		JButton btnChangeChildFolder = new JButton("Change Child Folder");
+		btnChangeChildFolder.setBounds(312, 72, 158, 23);
 		btnChangeChildFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ContentList ctl2 = new ContentList(Program.this, CurDir,2);
@@ -112,21 +126,21 @@ public class Program extends JFrame {
 			}
 		});
 		btnChangeChildFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnChangeChildFolder.setBounds(312, 72, 158, 23);
 		contentPane.add(btnChangeChildFolder);
 		
 		JLabel lblNewFolderName = new JLabel("New Folder Name:");
-		lblNewFolderName.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		lblNewFolderName.setBounds(454, 120, 142, 14);
+		lblNewFolderName.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		contentPane.add(lblNewFolderName);
 		
 		textNewDirName = new JTextField();
-		textNewDirName.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		textNewDirName.setBounds(454, 145, 142, 20);
+		textNewDirName.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		contentPane.add(textNewDirName);
 		textNewDirName.setColumns(10);
 		
 		JButton btnCreateNewFolder = new JButton("Create New Folder");
+		btnCreateNewFolder.setBounds(606, 144, 168, 23);
 		btnCreateNewFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String newDir = RTF(textNewDirName);
@@ -144,10 +158,10 @@ public class Program extends JFrame {
 			}
 		});
 		btnCreateNewFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnCreateNewFolder.setBounds(606, 144, 168, 23);
 		contentPane.add(btnCreateNewFolder);
 		
 		JButton btnDeleteCurFolder = new JButton("Delete Cur. Folder");
+		btnDeleteCurFolder.setBounds(606, 178, 168, 23);
 		btnDeleteCurFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				File newCurDir = CurDir.getParentFile();
@@ -162,8 +176,157 @@ public class Program extends JFrame {
 			}
 		});
 		btnDeleteCurFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnDeleteCurFolder.setBounds(606, 178, 168, 23);
 		contentPane.add(btnDeleteCurFolder);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 149, 413, 130);
+		contentPane.add(scrollPane);
+		
+		JTextArea textNewFileContent = new JTextArea();
+		scrollPane.setViewportView(textNewFileContent);
+		
+		JLabel lblNewFileName = new JLabel("New File Name:");
+		lblNewFileName.setBounds(454, 232, 142, 14);
+		lblNewFileName.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		contentPane.add(lblNewFileName);
+		
+		textNewFileName = new JTextField();
+		textNewFileName.setBounds(454, 257, 142, 20);
+		textNewFileName.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textNewFileName.setColumns(10);
+		contentPane.add(textNewFileName);
+		
+		JButton btnCreateNewFile = new JButton("Create New File");
+		btnCreateNewFile.setBounds(606, 257, 168, 23);
+		btnCreateNewFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String fileName = textNewFileName.getText();
+				if(fileName.length()==0) SM("File name is missing!",0);
+				else {
+					CreateFile(textNewFileContent, fileName);
+					textNewFileName.setText(null);
+					textNewFileContent.setText(null);
+					SM("File created!",1);
+				}
+			}
+		});
+		btnCreateNewFile.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		contentPane.add(btnCreateNewFile);
+		
+		JLabel lblNewFileContent = new JLabel("New File Content:");
+		lblNewFileContent.setBounds(10, 121, 142, 14);
+		lblNewFileContent.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		contentPane.add(lblNewFileContent);
+		
+		JButton btnDeleteFile = new JButton("Delete File");
+		btnDeleteFile.setBounds(606, 291, 168, 23);
+		btnDeleteFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ContentList ctl3 = new ContentList(Program.this, CurDir, 3);
+				ctl3.setVisible(true);
+			}
+		});
+		btnDeleteFile.setForeground(Color.RED);
+		btnDeleteFile.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		contentPane.add(btnDeleteFile);
+		
+		JLabel lblNewLabel = new JLabel("Source: Folder + File");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel.setBounds(10, 423, 272, 14);
+		contentPane.add(lblNewLabel);
+		
+		textSource = new JTextField("");
+		textSource.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textSource.setEditable(false);
+		textSource.setColumns(10);
+		textSource.setBackground(Color.WHITE);
+		textSource.setBounds(10, 446, 477, 20);
+		contentPane.add(textSource);
+		
+		JLabel lblDestinationFolderfile = new JLabel("Destination Folder (+File)");
+		lblDestinationFolderfile.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblDestinationFolderfile.setBounds(10, 477, 272, 14);
+		contentPane.add(lblDestinationFolderfile);
+		
+		textDestination = new JTextField("");
+		textDestination.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textDestination.setEditable(false);
+		textDestination.setColumns(10);
+		textDestination.setBackground(Color.WHITE);
+		textDestination.setBounds(10, 502, 477, 20);
+		contentPane.add(textDestination);
+		
+		JLabel lblCopyFile = new JLabel("Copy File:");
+		lblCopyFile.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblCopyFile.setBounds(10, 286, 272, 14);
+		contentPane.add(lblCopyFile);
+		
+		JLabel lblSelectThe = new JLabel("1. Select the source folder as the Current Dir");
+		lblSelectThe.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectThe.setBounds(10, 300, 413, 14);
+		contentPane.add(lblSelectThe);
+		
+		JLabel lblSelectThe_2 = new JLabel("2. Select the file from the folder, with Select File button");
+		lblSelectThe_2.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectThe_2.setBounds(10, 325, 413, 14);
+		contentPane.add(lblSelectThe_2);
+		
+		JLabel lblSelectThe_2_1 = new JLabel("3. Select the destionation folder for the current folder");
+		lblSelectThe_2_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectThe_2_1.setBounds(10, 350, 413, 14);
+		contentPane.add(lblSelectThe_2_1);
+		
+		JLabel lblSelectThe_2_1_1 = new JLabel("4. Press the Select button to select the current folder as the destination folder");
+		lblSelectThe_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectThe_2_1_1.setBounds(10, 375, 460, 14);
+		contentPane.add(lblSelectThe_2_1_1);
+		
+		JLabel lblSelectThe_2_1_1_1 = new JLabel("5. Press Copy File button to copy");
+		lblSelectThe_2_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSelectThe_2_1_1_1.setBounds(10, 400, 460, 14);
+		contentPane.add(lblSelectThe_2_1_1_1);
+		
+		JButton btnSelectFile = new JButton("Select File");
+		btnSelectFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ContentList ctl4 = new ContentList(Program.this,CurDir, 4);
+				ctl4.setVisible(true);
+				selectedFile = ctl4.getOutFile();
+				sourceFile = new File(CurDir.getPath()+ separator + selectedFile);
+				textSource.setText(sourceFile.getAbsolutePath());
+			}
+		});
+		btnSelectFile.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSelectFile.setBounds(512, 446, 168, 23);
+		contentPane.add(btnSelectFile);
+		
+		JButton btnSelectDestFolder = new JButton("Select Dest. Folder");
+		btnSelectDestFolder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				destFile = new File(CurDir.getPath()+ separator + selectedFile);
+				textDestination.setText(destFile.getAbsolutePath());
+			}
+		});
+		btnSelectDestFolder.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnSelectDestFolder.setBounds(512, 502, 168, 23);
+		contentPane.add(btnSelectDestFolder);
+		
+		JButton btnCopyFile = new JButton("Copy File");
+		btnCopyFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(textSource.getText().length() == 0)
+					SM("No source file selected!",0);
+				else if(textDestination.getText().length() == 0)
+					SM("No destinationi folder selected!",0);
+				else {
+					copyFile(sourceFile, destFile);
+					SM("File copied!",1);
+				}
+			}
+		});
+		btnCopyFile.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnCopyFile.setBounds(685, 474, 89, 23);
+		contentPane.add(btnCopyFile);
 	}
 	
 	public void SM(String msg, int type){
@@ -172,5 +335,35 @@ public class Program extends JFrame {
 	
 	public String RTF(JTextField jtf){
 		return jtf.getText();
+	}
+	
+	public void CreateFile(JTextArea jta, String fileName) {
+		fileName = CurDir.getAbsolutePath()+separator+fileName;
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream(fileName));
+			out.print(jta.getText());
+			out.close();
+			SM("Data is written to a file!",1);
+		}catch (IOException ioe) {
+			SM("CreateFile method: "+ioe.getMessage(),0);
+		}
+	}
+	
+	public void copyFile(File source, File dest) {
+		InputStream is = null;
+		OutputStream os = null;
+		
+		try {
+			is = new FileInputStream(source);
+			os = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			int length;
+			while((length = is.read(buffer)) > 0) {
+				os.write(buffer,0,length);
+			}
+			is.close();
+			os.close();
+		} catch(Exception e) {SM("copyFile: "+e.getMessage(),0);}
+		
 	}
 }
